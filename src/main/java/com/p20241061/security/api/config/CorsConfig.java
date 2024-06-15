c59e8dc5584ece51ab801520dec950936f91dcbe
Franco Carrillo Guevara <fcarrillo@baufest.com>
2024-06-15 11:17:34 -0500

    fix cors

--- src/main/java/com/p20241061/security/api/config/CorsConfig.java
+++ src/main/java/com/p20241061/security/api/config/CorsConfig.java
@@ -12,7 +12,7 @@ public class CorsConfig implements WebFluxConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
-                .allowedOrigins("*")
+                .allowedOrigins("http://localhost:4200")
                 .allowedHeaders("*")
                 .allowCredentials(true);
     }
